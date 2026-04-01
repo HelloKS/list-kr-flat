19e07eceed06de2ce20e428c62d47d03b81d9fcc
HelloKS <kqwe1859@gmail.com>
2026-04-01 19:38:06 +0900

    Update filterlist URL

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ import urllib.request
 import datetime
 import os
 
-uBO_filter_URL = 'https://raw.githubusercontent.com/List-KR/List-KR/master/filter-uBlockOrigin.txt'
+uBO_filter_URL = 'https://raw.githubusercontent.com/List-KR/List-KR/master/filterslists/filterslist-uBlockOrigin.txt'
 
 print("Filter update triggered at " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
